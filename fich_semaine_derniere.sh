e8c811f738906106fc7fc68f75df3db68a5cfaaa
Benedicte Nachampassak <benedicte.nachampassak@gmail.com>
2018-12-04 21:56:46 +0100

    Ajout du fichier .sh

---
+++ fich_semaine_derniere.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "Bonjour à toi, comment t'appelles-tu ? " user
+echo "Bienvenue $USERNAME, où plus particulièrement $user, il fait très froid en ce moment.. Nous sommes le $(date +%d) $(date +%B) $(date +%Y) !"
+echo ""
+
+cd 
+
+echo "Voici les fichiers que tu trouveras dans le répertoire sorbonne!"
+ls  
+
+read -p "Quel répertoire existant souhaites-tu ? " rep
+cd $rep
+echo""
+ 
+echo "Le chemin de ton répertoire est le suivant:" 
+pwd
+echo ""
+
+echo "Voici les 4 premières lignes du script:"
+head -n 4 fich_semaine_derniere.sh  
+
